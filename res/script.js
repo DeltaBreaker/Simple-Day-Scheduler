b9bfccaf94919e0d3e924c5b8f3fa1a8f2a3a3eb
willc <willcrain1@gmail.com>
2022-10-06 02:59:07 -0400

    Added buttons to save tasks which write to local storage

--- res/script.js
+++ res/script.js
@@ -23,5 +23,20 @@ for(var i = 9; i <= 17; i++) {
     textArea.attr("id", "input-field" + i);
     timeblock.append(textArea);
 
+    var saveButton = $("<button>");
+    saveButton.addClass("col-md-1 rounded");
+    saveButton.css("background-color", "#06AED4");
+    saveButton.text("Save");
+    saveButton.on("click", function() {
+        localStorage.setItem($(this).parent().children(".input-field").attr("id"), $(this).parent().children(".input-field").val());
+    });
+    saveButton.on("mouseover", function() {
+        $(this).css("background-color", "#0591AD");
+    });
+    saveButton.on("mouseout", function() {
+        $(this).css("background-color", "#06AED4");
+    });
+    timeblock.append(saveButton);
+
     timeblockContainer.append(timeblock);
 }
