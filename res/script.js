5a0fe53d0ea70800c1e8e8507511172bf0f47aef
willc <willcrain1@gmail.com>
2022-10-06 20:10:41 -0400

    Added comments to script.js

--- res/script.js
+++ res/script.js
@@ -1,10 +1,16 @@
+/* ------------------------------ Setup ------------------------------ */
 var timeblockContainer = $(".container");
 
+// This loops from 9 to 17 (inclusive) and creates the time block elements for the web page
 for(var i = 9; i <= 17; i++) {
+
+    // Create each time block container
     var timeblock = $("<timeblock>");
     timeblock.addClass("timeblock row form-group");
     timeblock.attr("id", i);
 
+    // Create the sub container that holds the time, text
+    // Depending on the time, AM or PM is added and is also converted to 12hr time
     var time = $("<time>");
     var timeText = "";
     if(i < 12) {
@@ -18,16 +24,19 @@ for(var i = 9; i <= 17; i++) {
     time.addClass("col-md-1 align-self-center");
     timeblock.append(time);
 
+    // Create and apply each text area
     var textArea = $("<textarea>");
     textArea.addClass("form-control col-md-10 h-100 input-field");
     textArea.attr("id", "input-field" + i);
     timeblock.append(textArea);
 
+    // Create save buttons that change color on mouseover
     var saveButton = $("<button>");
     saveButton.addClass("col-md-1 rounded");
     saveButton.css("background-color", "#06AED4");
     saveButton.text("Save");
     saveButton.on("click", function() {
+        // This gets the buttons parent then the input field child of that parent and stores its contents in local storage with the time ID as the key
         localStorage.setItem($(this).parent().children(".input-field").attr("id"), $(this).parent().children(".input-field").val());
     });
     saveButton.on("mouseover", function() {
@@ -38,9 +47,11 @@ for(var i = 9; i <= 17; i++) {
     });
     timeblock.append(saveButton);
 
+    // Appy each time block to the main page container
     timeblockContainer.append(timeblock);
 }
 
+// This loads each time block text from local storage and applies that to the respective time block text field
 for(var i = 9; i <= 17; i++) {
     var dataIn = localStorage.getItem("input-field" + i);
     if(dataIn !== null) {
@@ -49,13 +60,18 @@ for(var i = 9; i <= 17; i++) {
 }
 
 setupTimeInfo();
+/* ------------------------------------------------------------------- */
 
+/* ------------------------------ Functions ------------------------------ */
+// This removes each time class from the given time container so that a new one can be applied
 function removeTimeClasses(container) {
     container.removeClass("past");
     container.removeClass("present");
     container.removeClass("future");
 }
 
+// Applies a certain class to each time block based on its time ID relative to the current time
+// Also updates the main timer at the top to display the current time
 function setupTimeInfo() {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
@@ -63,7 +79,7 @@ function setupTimeInfo() {
         var timeblock = $("#" + i);
         removeTimeClasses(timeblock);
 
-        var currentTime = 13;
+        var currentTime = moment().hour();
 
         if(i > currentTime) {
             timeblock.addClass("future");
@@ -75,6 +91,8 @@ function setupTimeInfo() {
     }
 }
 
+// Calls the function above every 1000ms
 var colorShift = setInterval(function() {
    setupTimeInfo();
 }, 1000);
+/* ----------------------------------------------------------------------- */
